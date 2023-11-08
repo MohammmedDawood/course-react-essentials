eb6da34c05f07353db27fedc988ee0ecb2510195
Mohammed Dawood <mahmedtony73@gmail.com>
2023-11-08 11:50:17 +0200

    seperate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { CORE_CONCEPTS } from "./data";
+import CoreConcept from "./components/CoreConcept";
 import Header from "./components/Header";
 
 function App() {
@@ -5,7 +7,15 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id='core-concepts'>
+          <h1>Core Concepts</h1>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+          </ul>
+        </section>
       </main>
     </div>
   );
