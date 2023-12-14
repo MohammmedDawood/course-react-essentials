ac8d01a4987c6f0b4ba2d956e4f70d882c5186df
Mohammed Dawood <mahmedtony73@gmail.com>
2023-12-14 22:35:17 +0400

    Rendering Content Conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,23 @@ import Header from "./components/Header/Header";
 import TabButton from "./components/TabButton";
 
 function App() {
-  const [selectedTab, setSelectedTab] = useState("components");
+  const [selectedTab, setSelectedTab] = useState();
   function handleSelect(selectedTab) {
     console.log("select tab", selectedTab);
     setSelectedTab(selectedTab);
+  }
+
+  let tabContent = <p> Please select a tab to see the example.</p>;
+  if (selectedTab) {
+    tabContent = (
+      <div id='tab-content'>
+        <h3>{EXAMPLES[selectedTab].title}</h3>
+        <p>{EXAMPLES[selectedTab].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTab].code}</code>
+        </pre>
+      </div>
+    );
   }
   return (
     <div>
@@ -41,14 +54,17 @@ function App() {
             <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
             <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
-
-          <div id='tab-content'>
-            <h3>{EXAMPLES[selectedTab].title}</h3>
-            <p>{EXAMPLES[selectedTab].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTab].code}</code>
-            </pre>
-          </div>
+          {tabContent}
+          {/* {!selectedTab && <p> Please select a tab to see the example.</p>}
+          {selectedTab && (
+            <div id='tab-content'>
+              <h3>{EXAMPLES[selectedTab].title}</h3>
+              <p>{EXAMPLES[selectedTab].description}</p>
+              <pre>
+                <code>{EXAMPLES[selectedTab].code}</code>
+              </pre>
+            </div>
+          )} */}
         </section>
       </main>
     </div>
